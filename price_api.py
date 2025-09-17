c11587a954b7a6cea9dd21021dc608498d4b466d
kka <tsaiyuforwork@gmail.com>
2025-09-17 10:05:50 +0800

    update api

--- price_api.py
+++ price_api.py
@@ -1,6 +1,11 @@
 from flask import Flask, abort, request, jsonify
 from main_price import Price
 import pandas as pd
+import threading
+from check_user_comfort import background_check_loop, chek_user_questionnaire_status
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -9,9 +14,14 @@ app = Flask(__name__)
 def decision_price():
     register = request.get_json()
     data = pd.DataFrame(register)
-    decision, predicted = Price(data)
+    decision, predicted, woa_result, total_result = Price(data)
     #運算失敗會回傳400
     if decision['StatusCode'][0] == 200:
+        room_id = total_result["room_id"][0]
+        decision_remark = total_result["decision_remark"][0]
+        best_temp = woa_result["best_temp"][0]
+        best_humd = woa_result["best_humd"][0]
+        chek_user_questionnaire_status(room_id, decision_remark, best_temp, best_humd)
         return jsonify({
                         "StatusCode" : "200",
                         "ac_temp": decision['ac_temp'][0],
